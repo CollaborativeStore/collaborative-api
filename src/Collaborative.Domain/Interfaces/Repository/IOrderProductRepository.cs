ebd7024c6cefab3675e5a22676251eea12383995
AlessandroMaia <ale.maia.alm@gmail.com>
2020-09-12 09:30:52 -0300

    Add OrderProduct repository

---
+++ src/Collaborative.Domain/Interfaces/Repository/IOrderProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using Collaborative.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Collaborative.Domain.Interfaces.Repository
+{
+    public interface IOrderProductRepository
+    {
+        Task<IEnumerable<OrderProduct>> GetAllAsync();
+        Task<OrderProduct> GetByIdAsync(int id);
+    }
+}
---
+++ src/Collaborative.Infra/Repository/OrderProductRepository.cs
@@ -0,0 +1,41 @@
+﻿using Collaborative.Domain.Models;
+using Collaborative.Domain.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Collaborative.Infra.Context;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Collaborative.Infra.Repository
+{
+    public class OrderProductRepository : IOrderProductRepository
+    {
+        private EntityContext _entityContext;
+
+        public OrderProductRepository(EntityContext entityContext)
+        {
+            _entityContext = entityContext;
+        }
+
+        public async Task<IEnumerable<OrderProduct>> GetAllAsync()
+        {
+            var orderProducts = await _entityContext
+                .orderProducts
+                .ToListAsync();
+
+            return orderProducts;
+        }
+
+        public async Task<OrderProduct> GetByIdAsync(int id)
+        {
+            var orderProduct = await _entityContext
+                .orderProducts
+                .Where(x => x.Id.Equals(id))
+                .FirstOrDefaultAsync();
+
+            return orderProduct;
+        }
+    }
+}

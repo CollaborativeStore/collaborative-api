343a667024bcb4f66480ec6c97d4e871c19f4400
AlessandroMaia <ale.maia.alm@gmail.com>
2020-09-13 11:11:10 -0300

    Add Swagger config

---
+++ src/Collaborative.API/Swagger/SwaggerConfig.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace Collaborative.API.Swagger
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddSwaggerConfig(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options => {
+                options.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "CollabStore",
+                    Version = "1.0",
+                    Description = "WEB API CollabStore",
+                    Contact = new OpenApiContact() { Name = "Alessandro Maia", Email = $"ale.maia.alm@gmail.com", Url = new Uri("https://www.linkedin.com/in/alessandro-maia/") },
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+                });
+
+                //options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                //{
+                //    Description = "Insira o token JWT desta maneira: Bearer {seu token}",
+                //    Name = "Authorization",
+                //    In = ParameterLocation.Header,
+                //    Type = SecuritySchemeType.ApiKey
+                //});
+
+                //options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                //{
+                //    {
+                //        new OpenApiSecurityScheme
+                //        {
+                //            Reference = new OpenApiReference
+                //            {
+                //                Type = ReferenceType.SecurityScheme,
+                //                Id = "Bearer"
+                //            }
+                //        },
+                //        new string[] {}
+                //    }
+                //});
+            });
+
+            return services;
+        }
+
+
+        public static IApplicationBuilder UseSwaggerConfig(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(
+                options => {
+                    options.SwaggerEndpoint($"/swagger/v1/swagger.json", "Collab Version 1.0");
+                });
+            return app;
+        }
+    }
+}
